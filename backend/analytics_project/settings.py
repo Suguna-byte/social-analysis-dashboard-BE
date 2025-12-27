123ada6a91eee7a1ac79055bb12131ac2bd34dbf
Suguna P J <sugunajayarajan@gmail.com>
2025-12-27 12:48:36 +0530

    adding staticFiles

--- backend/analytics_project/settings.py
+++ backend/analytics_project/settings.py
@@ -8,7 +8,9 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-your-secret-key-chang
 
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1').split(',')
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,social-analysis-dashboard-be-production.up.railway.app,social-analytics-dashboard-be-production.up.railway.app').split(',')
+
+SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
 INSTALLED_APPS = [
     'django.contrib.admin',
